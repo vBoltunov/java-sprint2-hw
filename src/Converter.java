16d195e87e6bff097e3fe9b5fcd324e70f43a886
vBoltunov <vitaliy.boltunov@yandex.ru>
2024-06-23 23:50:56 +0300

    Add necessary comments to Converter class

--- src/Converter.java
+++ src/Converter.java
@@ -1,10 +1,11 @@
 public class Converter {
 
+    // 1 шаг = 75 см
     int convertToKm(int steps) {
-        double stepsInKilometers = steps * 0.75 / 1000;
-        return (int) stepsInKilometers;
+        return steps * 75 / 100 / 1000;
     }
 
+    // 1 шаг = 50 кал; 1 ккал = 1000 кал
     int convertStepsToKilocalories(int steps) {
         return steps * 50 / 1000;
     }
